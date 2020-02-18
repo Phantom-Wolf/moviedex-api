6d6497ad62fecb74a4c3621cfec5075192eda833
Mitchell Schmidt <MitchellT.S@outlook.com>
2020-02-17 23:45:15 -0600

    completed assignment

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 // imports
+
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
 const MOVIEDEX = require('./moviedex.json');
 
 const app = express();
@@ -9,12 +12,15 @@ const app = express();
 // middleware
 
 app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
 
 // authorization
 
 app.use(function validateBearerToken(req, res, next) {
 	const apiToken = process.env.API_TOKEN;
 	const authToken = req.get('Authorization');
+
 	if (!authToken || authToken.split(' ')[1] !== apiToken) {
 		return res.status(401).json({ error: 'Unauthorized request' });
 	}
